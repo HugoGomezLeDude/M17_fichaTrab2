1b6107a7bcfb5be557dd3bc46f190254683ae698
a15763 <a15763@d1-08-12.dped.local>
2017-02-08 10:29:32 +0000

    version Beta 0.2

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -1,5 +1,11 @@
 package m17b_fichatrab2;
 
+/**
+ * @version 0.2
+ * versão concepida por
+ * @author Hugo Pereira
+ * 
+ */
 public class Ponto2D {
     private int x;
     private int y;
@@ -9,22 +15,48 @@ public class Ponto2D {
         x = 0;
         y = 0;  
     }
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
+    /**
+     * 
+     * @return 
+     */
     public int getX(){
         return x;
     }
+    /**
+     * 
+     * @return 
+     */
     public int getY(){
         return y;
     }
+    /**
+     * 
+     * @param y 
+     */
     public void setY(int y){
         this.y=y;
     }
+    /**
+     * 
+     * @param x 
+     */
     public void setX(int x){
         this.x=x;
     }
+    /**
+     * 
+     * @param a
+     * @return 
+     */
     public double distancia(Ponto2D a){
         
         int totalX=0;
